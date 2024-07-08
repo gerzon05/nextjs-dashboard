ceb6d9b9d6da75af155b55e9ef32eef5fde87fcf
Gerzon05 <gerzonarletrangel@gmail.com>
2024-07-08 18:29:01 -0500

    feat: Update CardWrapper to accept a value prop

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -13,14 +13,14 @@ const iconMap = {
   invoices: InboxIcon,
 };
 
-export default async function CardWrapper() {
+export default async function CardWrapper({ value }: { value: number | string}) {
   return (
     <>
       {/* NOTE: Uncomment this code in Chapter 9 */}
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-      <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+      <Card title="Collected" value={value} type="collected" />
+      {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
+      {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
