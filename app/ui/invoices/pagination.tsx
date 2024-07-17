ee8d2bc9635c4f2b66c2b4faaec4ef46c711cb5b
Gerzon05 <gerzonarletrangel@gmail.com>
2024-07-17 09:49:27 -0500

    chore: Refactor createPageURL function in pagination.tsx

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -13,7 +13,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   const searchParams = useSearchParams()
   const currentPage = Number(searchParams.get('page')) || 1;
 
-  const createPageURL = (pageNumber: number | string)=>{
+  const createPageURL = (pageNumber: number | string) => {
     const params = new URLSearchParams(searchParams)
     params.set('page', pageNumber.toString())
     return `${pathname}?${params.toString()}`
